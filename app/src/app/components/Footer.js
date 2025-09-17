561c928f10a3b106c3383266b2e1e8f7cf2778dd
kristyson <kristyson.business@gmail.com>
2025-09-17 20:10:36 +0000

    Cabeçalho e apresentação.

---
+++ app/src/app/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        As alterações são salvas diretamente no Back4App, permitindo que toda a equipe acompanhe o cardápio em tempo real.
+      </p>
+    </footer>
+  );
+}
---
+++ app/src/app/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>DGK Restaurante - Gestão do Cardápio</h1>
+      <p>
+        Faça o controle completo dos pratos e acompanhe o clima para planejar promoções e eventos especiais.
+      </p>
+    </header>
+  );
+}
