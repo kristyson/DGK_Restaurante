7d0459e247b3fad46b2907dcf8227d3c3397037b
danielSuassuna <daniel.00000850857@unicap.br>
2025-09-18 15:27:49 -0300

    Update Header.js

--- app/src/app/components/Header.js
+++ app/src/app/components/Header.js
@@ -1,7 +1,9 @@
+import styles from "../page.module.css";
+
 export default function Header() {
   return (
-    <header>
-      <h1>DGK Restaurante - Gestão do Cardápio</h1>
+    <header className={styles.sectionHeader}>
+      <h2>DGK Restaurante - Gestão do Cardápio</h2>
       <p>
         Faça o controle completo dos pratos e acompanhe o clima para planejar promoções e eventos especiais.
       </p>
